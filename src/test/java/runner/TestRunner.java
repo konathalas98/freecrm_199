d7eb02b74c176ec8095ce32625d0017b32ce6af8
RaghuveerVundhyala <raghuvindhyala@gmail.com>
2020-12-09 13:25:19 +0530

    FreeCRM final

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -14,8 +14,8 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
 	
 		features = "Feature"
-		,glue={"stepDefinition"}
-		,tags={"@LoginTest"},
+		,glue={"stepDefinition"},
+		//tags={"@LoginTest"},
 		plugin = {"json:target/cucumber.json"}
 		//,monochrome=true //to display clear output on console in readable format
 		//,dryRun=true //checks the mapping between feature file and stepDef file
